967b788510e92ddddba365273e5fbd487b759c86
Justin Sainton <justinsainton@gmail.com>
2017-08-22 15:52:10 -0700

    Remove spinner when rates are returned

--- assets/js/shipstation.js
+++ assets/js/shipstation.js
@@ -14,8 +14,6 @@ window.ZWOOWH = window.ZWOOWH || {};
 	app.shipStation = {};
 	var ship = app.shipStation;
 
-	// ship.modalOpened = false;
-
 	ship.cache = function() {
 		app.$.get_rates_button = app.$get( 'get_shipstation_rates' );
 		app.$.set_rates_button = $( 'set_shipstation_rates' );
@@ -44,13 +42,14 @@ window.ZWOOWH = window.ZWOOWH || {};
 		$.post( window.ajaxurl, { action : 'get_shipstation_rates', order_id : ship.order_id }, function( response ) {
 			console.log( response );
 
-			var $select = $( '<select id="shipstation-rates" />' ).prependTo( app.$.get_rates_button );
+			var $select = $( '<select id="shipstation-rates" />' ).insertBefore( app.$.get_rates_button );
 
 			$.each( response.data, function( i, v ) {
 				$select.append( '<option data-price="' + v.shipmentCost.toFixed(2) + '" value="' + v.serviceCode + '">' + v.serviceName + ' - $' + v.shipmentCost.toFixed(2) + '</option>' );
 			} );
 
 			$select.select2();
+			app.$.shipSpinner.removeClass( 'is-active' );
 
 		}, 'json' );
 	};
